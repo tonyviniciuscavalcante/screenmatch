d051d93d39ad6ad8f6b8c707ff4969b29392fba1
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2025-04-10 21:48:57 -0300

    Update ScreenmatchApplication.java

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,12 +17,14 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=e42f0bf8");
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=e42f0bf8");
 		System.out.println(json);
 
 		// json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
 		// System.out.println(json);
 
-
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
